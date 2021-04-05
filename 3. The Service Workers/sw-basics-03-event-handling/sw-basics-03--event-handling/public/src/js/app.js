722e50f69e5ec2d6a0d0feb4b3b8c4f7c34f15f3
Dominik <dominikquiring@gmail.com>
2021-04-05 17:31:25 -0600

    just unzipped folders

---
+++ 3. The Service Workers/sw-basics-03-event-handling/sw-basics-03--event-handling/public/src/js/app.js
@@ -0,0 +1,8 @@
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js', {scope:'/help/'})
+    .then(function() {
+      console.log('Service worker registered!');
+    });
+}
